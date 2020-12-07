d580578e82da7505cde8c40081ad20600b093e6a
ckhuu <ckhuu@cs.ucalgary.ca>
2020-12-06 17:43:46 -0700

    Adding new customer to database via addCustomer.html

--- addCustomer.php
+++ addCustomer.php
@@ -1,74 +1,31 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'cpsc471hms');
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$phone = $_POST['phone'];
 
-// Create connection 
-$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME); 
+// only submit if all values are filled in
+if (!empty($fname) || !empty($lname) || !empty($phone)) {
+ $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "cpsc471hms";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
 
-// Check connection
-if($con === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-echo "add customer page";
-
-// query all customers
-// for debugging purposes
-$result = mysqli_query($con, "SELECT * FROM customer");
-echo "<table border='1'>
-<tr>
-<th>customerID</th>
-<th>Phone</th>
-<th>Fname</th>
-<th>Lname</th>
-</tr>";
-
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['Customer_id'] . "</td>";
-echo "<td>" . $row['Phone'] . "</td>";
-echo "<td>" . $row['Fname'] . "</td>";
-echo "<td>" . $row['Lname'] . "</td>";
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+      $INSERT = "INSERT Into customer (fname, lname, phone) values(?, ?, ?)";
+      //Prepare statement
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("sss", $fname, $lname, $phone);
+      $stmt->execute();
+      echo "New customer inserted sucessfully";
+      $stmt->close();
+      $conn->close();
+    }
+} else {
+ echo "Please fill in all fields.";
+ die();
 }
 ?>
-
-<html>
-<! -- Form to insert new customer -->
-<body>
-<div>
-<form action="addCustomer.php" method="post">
-First Name: <input type="text" name="fname">
-Last Name: <input type="text" name="lname">
-Phone number: <input type="text" name="phone">
-<input name="submitCustomer" type="submit">
-</form>
-</div>
-
-<div>
-<?
-// values to add to customer table
-// references name from text input
-$phone = $_POST["phone"];
-$fname = (string)$_POST["fname"];
-$lname = (string)$_POST["lname"];
-
-
-// after submit button is cliecked
-if (isset($_POST["submitCustomer"])) {
-  echo "Submit button pressed";
-  $sql = "INSERT INTO customer (`Phone`, `Fname`, `Lname`) VALUES ($phone, $fname, $lname)";
-  if (!mysqli_query($con,$sql)) {
-    die('Error: ' . mysqli_error($con));
-  }
-  else {
-    echo "1 record added";
-  }
-?>
-</div>
-</body>
-</html>
