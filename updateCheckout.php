4429e989a1d147871eb95507b0f68bb008837949
Hamza Gohar <hamzabitw6178@gmail.com>
2020-12-07 10:47:20 -0700

    Add files via upload

---
+++ updateCheckout.php
@@ -0,0 +1,18 @@
+<?php
+
+$ID= $_POST["Reservation ID"];
+$date = $_POST["date"];
+
+// Create connection
+$con=mysqli_connect("localhost","root","","hotel_mg");
+
+// Check connection
+if (mysqli_connect_errno($con))
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$sql = "UPDATE Reservation as R SET R.Checkout_date = '".$date."' Where  R.Reservation_id =" .$ID;
+$result = mysqli_query($con,$sql);
+mysqli_close($con);
+
+?>
