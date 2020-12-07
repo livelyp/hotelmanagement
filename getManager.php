e8ae9f942d21be08724278c1e8c8c9d4b2074bee
Hamza Gohar <hamzabitw6178@gmail.com>
2020-12-06 17:07:45 -0700

    Add files via upload

---
+++ getManager.php
@@ -0,0 +1,26 @@
+<?php 
+
+$con=mysqli_connect("localhost","root","","hotel_mg"); 
+
+if(mysqli_connect_errno($con)){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$result = mysqli_query($con,"SELECT M.Employee_id, E.name FROM MANAGER AS M, STAFF AS S, EMPLOYEE AS E WHERE M.Employee_id = S.Mgr_id AND M.Employee_id=E.Employee_id");
+
+echo "<table border='1'>
+<tr>
+<th>ID</th>
+<th>Name</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result)){
+    echo "<tr>";
+    echo "<td>" . $row['ID'] . "</td>";
+    echo "<td>" . $row['Name'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+
+?>
