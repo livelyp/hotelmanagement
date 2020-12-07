0e774e09fa4f5b61b7c0016086785466d65e7fda
Hamza Gohar <hamzabitw6178@gmail.com>
2020-12-07 10:35:20 -0700

    Add files via upload

---
+++ updatePoints.php
@@ -0,0 +1,19 @@
+<?php
+
+$ID= $_POST["Customer ID"];
+$points = $_POST["points"];
+$email = $_POST["email"];
+
+// Create connection
+$con=mysqli_connect("localhost","root","","hotel_mg");
+
+// Check connection
+if (mysqli_connect_errno($con))
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$sql = "UPDATE Member as M SET M.points = M.points + '".$points."' where M.Customer_id=" .$ID;
+$result = mysqli_query($con,$sql);
+mysqli_close($con);
+
+?>
