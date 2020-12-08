0fa7ca9e654254372ccb4e5d65e079fdca3e9a5a
ckhuu <ckhuu@cs.ucalgary.ca>
2020-12-07 19:05:57 -0700

    getManager returns a json encoding of data

--- getManager.php
+++ getManager.php
@@ -6,20 +6,24 @@ if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 $result = mysqli_query($con,"SELECT M.Employee_id, E.name FROM MANAGER AS M, STAFF AS S, EMPLOYEE AS E WHERE M.Employee_id = S.Mgr_id AND S.Employee_id = E.Employee_id");
+$arrayRow = array();
 
-echo "<table border='1'>
-<tr>
-<th>ID</th>
-<th>Name</th>
-</tr>";
+// echo "<table border='1'>
+// <tr>
+// <th>Employee_id</th>
+// <th>Name</th>
+// </tr>";
 
 while($row = mysqli_fetch_array($result)){
-    echo "<tr>";
-    echo "<td>" . $row['Employee_id'] . "</td>";
-    echo "<td>" . $row['name'] . "</td>";
-    echo "</tr>";
+    // echo "<tr>";
+    // echo "<td>" . $row['Employee_id'] . "</td>";
+    // echo "<td>" . $row['name'] . "</td>";
+    // echo "</tr>";
+    $arrayRow[] = $row;
 }
-echo "</table>";
+// echo "</table>";
+
+print(json_encode($arrayRow));
 
 mysqli_close($con);
 
