95a8ceaab2b7d81653eb08a584106011feb0e88c
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-08 13:10:32 -0700

    Complete Customer API CRUD

---
+++ api/customer/update.php
@@ -0,0 +1,25 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
+
+    include_once '../config/Database.php';
+
+    $database = New Database();
+    $db = $database->connect();
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $sql = "UPDATE customer SET Phone='$data[Phone]', Fname='$data[Fname]', Lname='$data[Lname]' WHERE Customer_id=$data[Customer_id]";
+
+    if ($db->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $db->error;
+    }
+
+    $db->close();
+
+?>
