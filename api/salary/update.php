14c597f0cde6eac14bbaeebbafbb18f4eb7a5466
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-08 14:02:48 -0700

    Salary CRUD

---
+++ api/salary/update.php
@@ -0,0 +1,25 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
+
+    include_once '../config/Database.php';
+
+    $database = New Database();
+    $db = $database->connect();
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $sql = "UPDATE salary SET Amount='$data[Amount]' WHERE Employee_id=$data[Employee_id]";
+
+    if ($db->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $db->error;
+    }
+
+    $db->close();
+
+?>
