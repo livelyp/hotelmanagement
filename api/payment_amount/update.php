b12c1d06066fbcc2319b6edf2600e2b2b6f3434f
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-08 15:09:38 -0700

    payment_amount CRUD

---
+++ api/payment_amount/update.php
@@ -0,0 +1,25 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
+
+    include_once '../config/Database.php';
+
+    $database = New Database();
+    $db = $database->connect();
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $sql = "UPDATE payment_amount SET Amount='$data[Amount]' WHERE Reservation_id=$data[Reservation_id] and Customer_id=$data[Customer_id]";
+
+    if ($db->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $db->error;
+    }
+
+    $db->close();
+
+?>
