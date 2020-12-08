a400ad7c7fec1e48e8a5fe788ecde8ac5e629507
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-08 14:22:45 -0700

    Room CRD

---
+++ api/room/read.php
@@ -0,0 +1,34 @@
+<?php
+    //headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    //get database
+    include_once '../config/Database.php';
+
+    $database = New Database();
+    $db = $database->connect();
+
+    //read
+    $sql = "SELECT * FROM room";
+
+    //formatting into json
+    if($result = $db -> query($sql)) {
+        $usr_arr = array();
+        $usr_arr['data'] = array();
+
+        while($row = $result->fetch_row()) {
+
+            $usr_item = array(
+                'Type_id' => $row[0],
+                'Room_num' => $row[1]
+            );
+
+            array_push($usr_arr['data'], $usr_item);
+        }
+
+        echo json_encode($usr_arr);
+    } else {
+        echo json_encode(array('message' => 'No Room Found'));
+    }
+?>
