03e1a27a72b345d752cdc3fc73612f06fb72081d
ckhuu <ckhuu@cs.ucalgary.ca>
2020-12-07 19:00:49 -0700

    Fixed error in SQL query

--- getManager.php
+++ getManager.php
@@ -1,11 +1,11 @@
 <?php 
 
-$con=mysqli_connect("localhost","root","","hotel_mg"); 
+$con=mysqli_connect("localhost","root","","cpsc471hms"); 
 
-if(mysqli_connect_errno($con)){
+if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-$result = mysqli_query($con,"SELECT M.Employee_id, E.name FROM MANAGER AS M, STAFF AS S, EMPLOYEE AS E WHERE M.Employee_id = S.Mgr_id AND M.Employee_id=E.Employee_id");
+$result = mysqli_query($con,"SELECT M.Employee_id, E.name FROM MANAGER AS M, STAFF AS S, EMPLOYEE AS E WHERE M.Employee_id = S.Mgr_id AND S.Employee_id = E.Employee_id");
 
 echo "<table border='1'>
 <tr>
@@ -15,8 +15,8 @@ echo "<table border='1'>
 
 while($row = mysqli_fetch_array($result)){
     echo "<tr>";
-    echo "<td>" . $row['ID'] . "</td>";
-    echo "<td>" . $row['Name'] . "</td>";
+    echo "<td>" . $row['Employee_id'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
     echo "</tr>";
 }
 echo "</table>";
