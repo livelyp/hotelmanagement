796f361897a2cc710994c2a0e5d9fcc2a6b1b5ab
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 15:39:29 -0700

    Update create.php

--- api/customer/create.php
+++ api/customer/create.php
@@ -1,5 +1,7 @@
 <?php
+    // File to create a customer enitity
 
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
@@ -7,13 +9,17 @@
 
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input to an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // Insert Phone, Fname, Lname into database Customer_id auto-increments
     $sql = "INSERT INTO customer(Phone, Fname, Lname) VALUES('$data[Phone]', '$data[Fname]', '$data[Lname]')";
 
+    // Ouput reult of create operation
     if(mysqli_query($db, $sql)) {
         echo "New customer created successfully";
     } else {
