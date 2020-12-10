589ff23b6c48bbd9acd95cb7a7158f741eadc283
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 15:32:59 -0700

    Update Database.php

--- api/config/Database.php
+++ api/config/Database.php
@@ -1,14 +1,21 @@
 <?php
     class Database {
+        /* 
+            set the host, database, username, and password in one file
+            if any change only one file will need to be changed
+        */
         private $host = 'localhost';
         private $db_name = 'cpsc471-hms';
         private $username = 'root';
         private $password = '';
         private $conn;
 
+        // function to connect to the database
         public function connect() {
+            // create a connection
             $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
 
+            // Output error if connection fails
             if(!($this->conn)){
                 die("Connection failed: " . mysqli_connect_error());
             }
