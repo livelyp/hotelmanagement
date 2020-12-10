5b6a8c17a08f64538397c1620950a0c9f625f51d
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 15:49:50 -0700

    Update delete.php

--- api/employee/delete.php
+++ api/employee/delete.php
@@ -1,23 +1,30 @@
 <?php
+    // File to delete employee entity
+
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input for employee id
     $data = json_decode(file_get_contents("php://input"), true);
 
-    // sql to delete a record
+    // sql to delete a employee matching employee id input
     $sql = "DELETE FROM employee WHERE Employee_id=$data[Employee_id]";
 
+    // Output result of delete operaton
     if ($db->query($sql) === TRUE) {
-        echo "Record deleted successfully";
+        echo "Employee deleted successfully";
     } else {
-        echo "Error deleting record: " . $db->error;
+        echo "Error deleting employee: " . $db->error;
     }
 
     $db->close();
