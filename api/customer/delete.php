5acaf723a92aaecacb94645a9d3a7f08fc8cd006
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 15:40:49 -0700

    Update delete.php

--- api/customer/delete.php
+++ api/customer/delete.php
@@ -1,4 +1,7 @@
 <?php
+    //File to delete customer entity
+
+    //Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
@@ -6,14 +9,17 @@
 
     include_once '../config/Database.php';
 
+    //Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json into custome_id
     $data = json_decode(file_get_contents("php://input"), true);
 
-    // sql to delete a record
+    // sql to delete customer with matching customer_id
     $sql = "DELETE FROM customer WHERE Customer_id=$data[Customer_id]";
 
+    // Output result of delete operation
     if ($db->query($sql) === TRUE) {
         echo "Record deleted successfully";
     } else {
