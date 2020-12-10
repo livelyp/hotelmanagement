461214ef9be0332d6f94736e1dd3eb540f4c530c
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 15:23:00 -0700

    Update create.php

--- api/cancel/create.php
+++ api/cancel/create.php
@@ -1,5 +1,7 @@
 <?php
 
+    // File to create a cancel relation
+
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
@@ -7,13 +9,17 @@
 
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode the json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // Insert the reservation_id and customer_id into database cancellation_id is auto-incremented 
     $sql = "INSERT INTO cancel(Reservation_id, Customer_id) VALUES('$data[Reservation_id]', '$data[Customer_id]')";
 
+    // Ouput the result of create operation
     if(mysqli_query($db, $sql)) {
         echo "New Cancellation created successfully";
     } else {
