6dee8863ca8416995e7fd109caff5b30972c62f0
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 16:12:25 -0700

    Update delete.php

--- api/guest/delete.php
+++ api/guest/delete.php
@@ -1,19 +1,26 @@
 <?php
+    // File to delete a guest from the database
+
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    //Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // decode the json to get customer id
     $data = json_decode(file_get_contents("php://input"), true);
 
-    // sql to delete a record
+    // sql to delete a guest with corresponding customer id
     $sql = "DELETE FROM guest WHERE Customer_id=$data[Customer_id]";
 
+    // Output result of delete operation
     if ($db->query($sql) === TRUE) {
         echo "Record deleted successfully";
     } else {
