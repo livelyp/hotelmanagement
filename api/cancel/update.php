4da3f6f980abf61fa6943755eb633463f835ca86
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 15:29:41 -0700

    Update update.php

--- api/cancel/update.php
+++ api/cancel/update.php
@@ -1,4 +1,5 @@
 <?php
+    //File to update a cancel relation
 
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
@@ -7,17 +8,21 @@
 
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode the json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // Update the cancel relation with matching cancellation_id
     $sql = "UPDATE cancel SET Reservation_id='$data[Reservation_id]', Customer_id='$data[Customer_id]' WHERE Cancellation_id=$data[Cancellation_id]";
 
+    // Output the result of the update realtion
     if ($db->query($sql) === TRUE) {
-        echo "Record updated successfully";
+        echo "Cancel relation updated successfully";
     } else {
-        echo "Error updating record: " . $db->error;
+        echo "Error updating cancel relation: " . $db->error;
     }
 
     $db->close();
