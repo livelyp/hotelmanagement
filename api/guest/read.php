2c11cd4a892b493e3f4e35144aa04821255e76cd
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 16:13:32 -0700

    Update read.php

--- api/guest/read.php
+++ api/guest/read.php
@@ -1,4 +1,6 @@
 <?php
+    // File to read all the guests in the database
+
     //headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
@@ -6,13 +8,14 @@
     //get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
-    //read
+    // Read all guests from database
     $sql = "SELECT * FROM guest";
 
-    //formatting into json
+    //formatting guest data into json
     if($result = $db -> query($sql)) {
         $usr_arr = array();
         $usr_arr['data'] = array();
@@ -25,7 +28,8 @@
 
             array_push($usr_arr['data'], $usr_item);
         }
-
+        
+        // Output the guests in json
         echo json_encode($usr_arr);
     } else {
         echo json_encode(array('message' => 'No Guests Found'));
