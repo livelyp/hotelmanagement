f0f81528650896f7d15f38ef6fd84f3dfa7f56b7
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 15:10:33 -0700

    Update delete.php

--- api/amenity_used/delete.php
+++ api/amenity_used/delete.php
@@ -1,4 +1,7 @@
 <?php
+
+    // File to delete a used amenity relation
+
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
@@ -6,18 +9,21 @@
 
     include_once '../config/Database.php';
 
+    // Connect to the database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode the json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
-    // sql to delete a record
+    // sql to delete a used amenity relation matching the input
     $sql = "DELETE FROM amenity_used WHERE Customer_id=$data[Customer_id] and Number=$data[Number] and Type='$data[Type]'";
 
+    // Output the result of the delete operation
     if ($db->query($sql) === TRUE) {
-        echo "Record deleted successfully";
+        echo "Used amenity relation deleted successfully";
     } else {
-        echo "Error deleting record: " . $db->error;
+        echo "Error deleting used amenity relation: " . $db->error;
     }
 
     $db->close();
