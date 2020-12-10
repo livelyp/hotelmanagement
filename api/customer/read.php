f1f2fdb05d88a0b65d3553f4e02061dd11a8d1fc
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 15:42:07 -0700

    Update read.php

--- api/customer/read.php
+++ api/customer/read.php
@@ -1,4 +1,6 @@
 <?php
+    // File to read all customer entities
+
     //headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
@@ -6,13 +8,14 @@
     //get database
     include_once '../config/Database.php';
 
+    // Connect to databse
     $database = New Database();
     $db = $database->connect();
 
-    //read
+    // Read all attributes from customer
     $sql = "SELECT * FROM customer";
 
-    //formatting into json
+    //formatting all customer sql into json
     if($result = $db -> query($sql)) {
         $usr_arr = array();
         $usr_arr['data'] = array();
@@ -29,6 +32,7 @@
             array_push($usr_arr['data'], $usr_item);
         }
 
+        // Output customer data as json
         echo json_encode($usr_arr);
     } else {
         echo json_encode(array('message' => 'No Customers Found'));
