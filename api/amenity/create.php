363673eda110f482de699b21391d142f98f197f2
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-10 14:59:27 -0700

    Update create.php

--- api/amenity/create.php
+++ api/amenity/create.php
@@ -1,5 +1,5 @@
 <?php
-
+    // File to create an ammenity in the database
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
@@ -7,13 +7,17 @@
 
     include_once '../config/Database.php';
 
+    //Connect to the database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode the json file from the api input into array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // Insert amenity number and type into database
     $sql = "INSERT INTO amenity(Number, Type) VALUES('$data[Number]', '$data[Type]')";
 
+    // Output the result of the insertion
     if(mysqli_query($db, $sql)) {
         echo "New amenities created successfully";
     } else {
