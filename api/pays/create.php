a08e3edfa6be13388568fbf2ab1cf213eec88635
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 18:41:52 -0700

    Update create.php

--- api/pays/create.php
+++ api/pays/create.php
@@ -1,19 +1,26 @@
 <?php
+    // Create a payment relation between manager and employee
 
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // SQL insert manager id and employee id into database
     $sql = "INSERT INTO pays(Mgr_id, Employee_id) VALUES('$data[Mgr_id]', '$data[Employee_id]')";
 
+    // Output the result of the insert operation
     if(mysqli_query($db, $sql)) {
         echo "New pays relation created successfully";
     } else {
