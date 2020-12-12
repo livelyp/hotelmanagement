9cf20bd9be1e0ccfec7ea0d1cba19cbfa952e17f
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 18:14:04 -0700

    Update update.php

--- api/member/update.php
+++ api/member/update.php
@@ -1,19 +1,26 @@
 <?php
+    // File to update member information in database
 
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // Update memebr information where customer id matches input
     $sql = "UPDATE member SET Username='$data[Username]', Pword='$data[Pword]', Points='$data[Points]' WHERE Customer_id=$data[Customer_id]";
 
+    // Output result of the update operation
     if ($db->query($sql) === TRUE) {
         echo "Record updated successfully";
     } else {
