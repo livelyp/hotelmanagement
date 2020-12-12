12e34fbd8d4cadd185b65a8dfdfc254eae6ac63a
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 18:25:24 -0700

    Update delete.php

--- api/payment_amount/delete.php
+++ api/payment_amount/delete.php
@@ -1,19 +1,26 @@
 <?php
+    // File to delete a customers payment amount
+
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
-    // sql to delete a record
+    // sql to delete a payment amount for a customers reservation
     $sql = "DELETE FROM payment_amount WHERE Reservation_id=$data[Reservation_id] and Customer_id=$data[Customer_id]";
 
+    // Output the result of the delete operation
     if ($db->query($sql) === TRUE) {
         echo "Record deleted successfully";
     } else {
