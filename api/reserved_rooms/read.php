5dc7e41d9f92a74e291546aa5a5f67de27f3a242
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 19:13:57 -0700

    Update read.php

--- api/reserved_rooms/read.php
+++ api/reserved_rooms/read.php
@@ -1,4 +1,6 @@
 <?php
+    // Read all the reserved rooms
+
     //headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
@@ -6,13 +8,14 @@
     //get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
-    //read
+    //read all attributes from reserved rooms
     $sql = "SELECT * FROM reserved_rooms";
 
-    //formatting into json
+    //formatting all reserved rooms into json
     if($result = $db -> query($sql)) {
         $usr_arr = array();
         $usr_arr['data'] = array();
@@ -28,6 +31,7 @@
             array_push($usr_arr['data'], $usr_item);
         }
 
+        // Output the reserved rooms in json
         echo json_encode($usr_arr);
     } else {
         echo json_encode(array('message' => 'No Reserved Rooms Found'));
