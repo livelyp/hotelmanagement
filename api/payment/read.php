c871574d580c624aa879bde8afbdf74bc8700368
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 18:21:09 -0700

    Update read.php

--- api/payment/read.php
+++ api/payment/read.php
@@ -1,4 +1,6 @@
 <?php
+    // File to read all payments in database
+
     //headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
@@ -6,13 +8,14 @@
     //get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
-    //read
+    // Read all attributes from payment
     $sql = "SELECT * FROM payment";
 
-    //formatting into json
+    //formatting all payments into json
     if($result = $db -> query($sql)) {
         $usr_arr = array();
         $usr_arr['data'] = array();
@@ -27,6 +30,7 @@
             array_push($usr_arr['data'], $usr_item);
         }
 
+        // Output all payments as json
         echo json_encode($usr_arr);
     } else {
         echo json_encode(array('message' => 'No Payments Found'));
