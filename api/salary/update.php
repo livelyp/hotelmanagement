5861313eb5ae1ead1d742411210f0ac51c4175e4
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 19:31:06 -0700

    Update update.php

--- api/salary/update.php
+++ api/salary/update.php
@@ -1,23 +1,30 @@
 <?php
+    // Update an employees salary
 
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // Update salary amount
     $sql = "UPDATE salary SET Amount='$data[Amount]' WHERE Employee_id=$data[Employee_id]";
 
+    // Output the result of update operation
     if ($db->query($sql) === TRUE) {
-        echo "Record updated successfully";
+        echo "Salary updated successfully";
     } else {
-        echo "Error updating record: " . $db->error;
+        echo "Error updating salary: " . $db->error;
     }
 
     $db->close();
