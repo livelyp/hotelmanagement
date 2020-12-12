911e292484fd318e34f75ccbc61777711964bb7e
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 19:16:28 -0700

    Update create.php

--- api/room/create.php
+++ api/room/create.php
@@ -1,19 +1,26 @@
 <?php
+    // Add a room to the database
 
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // Insert room type and number into database
     $sql = "INSERT INTO room(Type_id, Room_num) VALUES('$data[Type_id]', '$data[Room_num]')";
 
+    // Output the result of insert operation
     if(mysqli_query($db, $sql)) {
         echo "New room created successfully";
     } else {
