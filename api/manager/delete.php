bd633258b5c88f1499e96723343dfe3b6652a76d
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 17:55:35 -0700

    Update delete.php

--- api/manager/delete.php
+++ api/manager/delete.php
@@ -1,19 +1,26 @@
 <?php
+    // File to delete a manager from the database
+
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
-    // sql to delete a record
+    // sql to delete a manager from database
     $sql = "DELETE FROM manager WHERE Employee_id=$data[Employee_id]";
 
+    // Output the result of the delete operation
     if ($db->query($sql) === TRUE) {
         echo "Record deleted successfully";
     } else {
