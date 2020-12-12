70bc036882a53af6746c30ee7e51797ccec1b923
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 18:59:57 -0700

    Update update.php

--- api/reservation/update.php
+++ api/reservation/update.php
@@ -1,19 +1,26 @@
 <?php
+    // File to update a reservation
 
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
-
+    
+    // Update arrival and checkout dates for reservation 
     $sql = "UPDATE reservation SET Arrival_date='$data[Arrival_date]', Checkout_date='$data[Checkout_date]' WHERE Reservation_id=$data[Reservation_id]";
 
+    // Output the result of the update operation
     if ($db->query($sql) === TRUE) {
         echo "Record updated successfully";
     } else {
