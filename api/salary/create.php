4dd9cbc2863a201135e2ebb3ff851e79d8d057b3
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 19:26:42 -0700

    Update create.php

--- api/salary/create.php
+++ api/salary/create.php
@@ -1,19 +1,26 @@
 <?php
+    // Create a new salary for employee
 
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // Insert employee id and salary amount
     $sql = "INSERT INTO salary(Employee_id, Amount) VALUES('$data[Employee_id]', '$data[Amount]')";
 
+    // Output the result of insert operation
     if(mysqli_query($db, $sql)) {
         echo "New salary created successfully";
     } else {
