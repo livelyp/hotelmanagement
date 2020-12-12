67556a556591c5ab808cbce280467b9c33ac5922
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 18:11:00 -0700

    Update read.php

--- api/member/read.php
+++ api/member/read.php
@@ -1,4 +1,6 @@
 <?php
+    // File to Read all the members in the database
+
     //headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
@@ -6,13 +8,14 @@
     //get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
-    //read
+    // SQL read all attributes from member
     $sql = "SELECT * FROM member";
 
-    //formatting into json
+    //formatting member information into json
     if($result = $db -> query($sql)) {
         $usr_arr = array();
         $usr_arr['data'] = array();
@@ -29,6 +32,7 @@
             array_push($usr_arr['data'], $usr_item);
         }
 
+        // Output all members in json
         echo json_encode($usr_arr);
     } else {
         echo json_encode(array('message' => 'No Members Found'));
