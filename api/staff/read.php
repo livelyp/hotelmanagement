145bcddbaf2852a1bf48686e4fbb9b8a9bb23852
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 20:41:40 -0700

    Update read.php

--- api/staff/read.php
+++ api/staff/read.php
@@ -1,4 +1,6 @@
 <?php
+    // Read  all staff members in database
+
     //headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
@@ -6,13 +8,14 @@
     //get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
-    //read
+    //read all attributes from staff
     $sql = "SELECT * FROM staff";
 
-    //formatting into json
+    //formatting all staff into json
     if($result = $db -> query($sql)) {
         $usr_arr = array();
         $usr_arr['data'] = array();
@@ -27,6 +30,7 @@
             array_push($usr_arr['data'], $usr_item);
         }
 
+        // Output all staff in json
         echo json_encode($usr_arr);
     } else {
         echo json_encode(array('message' => 'No Staff Found'));
