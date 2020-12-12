318691d2770bbd8c452580f3dceca0ff5891e0ed
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 19:22:11 -0700

    Update delete.php

--- api/room_type/delete.php
+++ api/room_type/delete.php
@@ -1,23 +1,30 @@
 <?php
+    // Delete a room type from database
+
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
-    // sql to delete a record
+    // sql to delete a room type
     $sql = "DELETE FROM room_type WHERE Type_id=$data[Type_id]";
 
+    // Output the result of delete operation
     if ($db->query($sql) === TRUE) {
-        echo "Record deleted successfully";
+        echo "Room type deleted successfully";
     } else {
-        echo "Error deleting record: " . $db->error;
+        echo "Error deleting room type: " . $db->error;
     }
 
     $db->close();
