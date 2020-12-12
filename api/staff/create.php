a420fd2fa2fbf61a8636f27d6512e3750b6816e4
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 20:39:49 -0700

    Update create.php

--- api/staff/create.php
+++ api/staff/create.php
@@ -1,19 +1,26 @@
 <?php
+    // File to create a staff employee
 
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
-
+    
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // sql insert staff into database
     $sql = "INSERT INTO staff(Employee_id, Mgr_id) VALUES('$data[Employee_id]', '$data[Mgr_id]')";
 
+    // Output result of insert operation
     if(mysqli_query($db, $sql)) {
         echo "New staff created successfully";
     } else {
