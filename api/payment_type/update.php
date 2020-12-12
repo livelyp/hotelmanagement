74b53b8aa9a1f3a5888bf4e184ddc076034c7374
livelyp <47401496+livelyp@users.noreply.github.com>
2020-12-11 18:35:55 -0700

    Update update.php

--- api/payment_type/update.php
+++ api/payment_type/update.php
@@ -1,19 +1,26 @@
 <?php
+    // File to update a customer reservation payment type
 
+    // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
+    // Get database
     include_once '../config/Database.php';
 
+    // Connect to database
     $database = New Database();
     $db = $database->connect();
 
+    // Decode json input into an array data
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // SQL the payment type made by customer for their reservation
     $sql = "UPDATE payment_type SET Type='$data[Type]' WHERE Reservation_id=$data[Reservation_id] and Customer_id=$data[Customer_id]";
 
+    // Ouput the result of the update operation
     if ($db->query($sql) === TRUE) {
         echo "Record updated successfully";
     } else {
